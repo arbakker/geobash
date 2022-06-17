3327d0d9b43090c87c6d6fc9960efbf1530a7e62
Anton Bakker <anton.bakker@kadaster.nl>
2022-06-17 14:26:39 +0200

    fix shellcheck

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Requires `libproj-dev`, `gdal-bin`  and `perl` to be installed.
 
 Add directory to PATH (add to `.bashrc` or `.zshrc`):
 
-```
+```sh
 export PATH=${geobash_installation_path}:$PATH
 ```
 
@@ -24,20 +24,22 @@ Source files are linted with [Shellcheck](https://github.com/koalaman/shellcheck
 
 Create GPKG with geometries of all tiles of zoomlevel 3 (nr of tiles per zoomlevel is 2^z, zoomlevel z is zero-based index) of Dutch tiling scheme (EPSG:28992):
 
-```
+```sh
 zxy2bbox 28992 4/{0..15}/{0..15}  | bbox2wkt | wkt2ogr -  28992 GPKG grid.gpkg grid
 ```
+
 ----
 
 Show tile boundaries of specific tile (Dutch tiling scheme; EPSG:28992):
 
-```
+```sh
 zxy2bbox 28992 2/2/1  | bboxproj 28992 4326 - | bboxshow
 ```
+
 ----
 
 Calculate scale denominator of WMS GetMap request:
 
-```
+```sh
 wmsmap2scaledenominator https://service.pdok.nl/hwh/luchtfotorgb/wms/v1_0?SERVICE=WMS&REQUEST=GetMap&VERSION=1.3.0&LAYERS=Actueel_ortho25&BBOX=233328.32,554123.2,233382.08,554176.96&WIDTH=256&HEIGHT=256&CRS=EPSG:28992&FORMAT=image/png
 ```
--- schemadiff.sh
+++ schemadiff.sh
@@ -23,13 +23,13 @@ function usage {
 if test "$#" -lt 4; then
     usage
 fi
-if ! grep -E  -q "^(false|true)$" <<< $COMPLETE;then
+if ! grep -E -q "^(false|true)$" <<<"$COMPLETE"; then
     usage
 fi
 
 set -euo pipefail
 
-function get-ogr-geom-att(){
+function get-ogr-geom-att() {
     filepath=$1
     layer=$2
     geom_att_name=$(ogrinfo "$filepath" "$layer" -so | grep "Geometry Column" | cut -d= -f2 | xargs)
@@ -37,35 +37,37 @@ function get-ogr-geom-att(){
     echo "${geom_att_name}: ${geom_att_type}"
 }
 
-function get-ogr-attributes(){
+function get-ogr-attributes() {
     filepath=$1
     layer=$2
     complete=${3:-true}
     match=1
-    geom_att=$(get-ogr-geom-att "$filepath" "$layer" )
+    geom_att=$(get-ogr-geom-att "$filepath" "$layer")
+    # shellcheck disable=SC2046
+    # shellcheck disable=SC2015
     while read -r line; do
-        if $(grep -q "Geometry Column" <<< "$line");then
+        if grep -q "Geometry Column" <<<"$line"; then
             match=0
             continue
         fi
-        if [[ $match -eq 0 ]];then
-            echo $line
+        if [[ $match -eq 0 ]]; then
+            echo "$line"
         fi
-    done<<<$(ogrinfo -so "$filepath" "$layer" ) | sort | 
+    done <<<$(ogrinfo -so "$filepath" "$layer") | sort |
         (
-            [ $complete == 'false' ] && xargs -i echo '{}' | cut -d" " -f1,2 - || xargs -i echo '{}'
+            [ "$complete" == 'false' ] && xargs -I{} echo '{}' | cut -d" " -f1,2 - || xargs -I{} echo '{}'
         )
     echo "${geom_att}"
 }
 
-function compare-attributes-layers(){
+function compare-attributes-layers() {
     filepath_1=$1
     layer_1=$2
     filepath_2=$3
     layer_2=$4
     complete=${5:-true}
     layer1_attributes=$(get-ogr-attributes "$filepath_1" "$layer_1" "$complete")
-    layer2_attributes=$(get-ogr-attributes "$filepath_2" "$layer_2" "$complete")    
+    layer2_attributes=$(get-ogr-attributes "$filepath_2" "$layer_2" "$complete")
     echo "diff ${filepath_1}:${layer_1} - ${filepath_2}:${layer_2}"
     diff <(echo "$layer1_attributes") <(echo "$layer2_attributes")
 }
